f1b8be5641367ab51c3d53868900b31ad9c24776
DeaDGxD <59544724+DeaDGxD@users.noreply.github.com>
2020-01-25 17:15:39 -0600

    Update coinflip.py

--- coinflip.py
+++ coinflip.py
@@ -1,31 +1,16 @@
-import random, sys
-while True:
-    coin = ["heads", "tails"]
-    toss = 1
-    flips = input("How many times do you want to flip?" + "\n")
-    results = []
-    try:
+import random
+def coinflip(flips):
+    running = True
+    while running:
+        coin = ["heads", "tails"]
+        toss = 1
+        results = []
         while toss != int(flips):
             result = random.choice(coin)
             results += result
             toss += 1
-            print(result)
-    except ValueError:
-        if flips == "":
-            print("You need to enter a value.")
-            continue
-        else:
-            print("Only numbers will work!")
-            continue
-    heads = results.count("h")
-    tails = results.count("t")
-    flip = tails > heads
-    if flip == True:
-        print("Tails wins!")
-    else:
-        print("Heads wins!")
-    cat = input("Play agian? y/n" + "\n")
-    if cat == "y":
-        continue
-    else:
-        break
+        heads = results.count("h")
+        tails = results.count("t")
+        flip = tails > heads
+        running = False
+        return flip
